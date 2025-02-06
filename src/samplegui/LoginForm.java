116da6deea9749d543b4865dcbd1adb78649ab2b
YvanClarc <yonzonivanclark@gmail.com>
2025-02-06 16:46:17 +0800

    tae ni sir mike

--- src/samplegui/LoginForm.java
+++ src/samplegui/LoginForm.java
@@ -83,7 +83,7 @@ public class LoginForm extends javax.swing.JFrame {
         MainPanel.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 200, 210, 30));
 
         jButton1.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 12)); // NOI18N
-        jButton1.setText("CANCEL");
+        jButton1.setText("EXIT");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -143,11 +143,10 @@ public class LoginForm extends javax.swing.JFrame {
     }//GEN-LAST:event_jPasswordField1ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+            System.exit(0);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jLabel4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel4MouseClicked
--- src/samplegui/RegistrationForm.java
+++ src/samplegui/RegistrationForm.java
@@ -5,6 +5,8 @@
  */
 package samplegui;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Admin
@@ -16,6 +18,7 @@ public class RegistrationForm extends javax.swing.JFrame {
      */
     public RegistrationForm() {
         initComponents();
+        this.setResizable(false);
     }
 
     /**
@@ -32,16 +35,16 @@ public class RegistrationForm extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        ln = new javax.swing.JTextField();
         cancel = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        username = new javax.swing.JTextField();
         jLabel10 = new javax.swing.JLabel();
-        jPasswordField3 = new javax.swing.JPasswordField();
+        password = new javax.swing.JPasswordField();
         jLabel11 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
+        email = new javax.swing.JTextField();
+        fn = new javax.swing.JTextField();
         jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -69,14 +72,14 @@ public class RegistrationForm extends javax.swing.JFrame {
         jLabel5.setText("First Name:");
         MainPanel.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 160, -1, -1));
 
-        jTextField2.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
-        jTextField2.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+        ln.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
+        ln.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        ln.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
+                lnActionPerformed(evt);
             }
         });
-        MainPanel.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 200, 210, 30));
+        MainPanel.add(ln, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 200, 210, 30));
 
         cancel.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 12)); // NOI18N
         cancel.setText("CANCEL");
@@ -97,51 +100,51 @@ public class RegistrationForm extends javax.swing.JFrame {
         jLabel8.setText("Password: ");
         MainPanel.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 280, -1, -1));
 
-        jTextField3.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
-        jTextField3.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+        username.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
+        username.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        username.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField3ActionPerformed(evt);
+                usernameActionPerformed(evt);
             }
         });
-        MainPanel.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 240, 210, 30));
+        MainPanel.add(username, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 240, 210, 30));
 
         jLabel10.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
         jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel10.setText("Password: ");
         MainPanel.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 280, -1, -1));
 
-        jPasswordField3.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
-        jPasswordField3.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jPasswordField3.addActionListener(new java.awt.event.ActionListener() {
+        password.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
+        password.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        password.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jPasswordField3ActionPerformed(evt);
+                passwordActionPerformed(evt);
             }
         });
-        MainPanel.add(jPasswordField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 280, 210, 30));
+        MainPanel.add(password, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 280, 210, 30));
 
         jLabel11.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
         jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel11.setText("Email:");
         MainPanel.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 320, -1, -1));
 
-        jTextField4.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
-        jTextField4.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+        email.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
+        email.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        email.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField4ActionPerformed(evt);
+                emailActionPerformed(evt);
             }
         });
-        MainPanel.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 320, 210, 30));
+        MainPanel.add(email, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 320, 210, 30));
 
-        jTextField5.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
-        jTextField5.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jTextField5.addActionListener(new java.awt.event.ActionListener() {
+        fn.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 18)); // NOI18N
+        fn.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        fn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField5ActionPerformed(evt);
+                fnActionPerformed(evt);
             }
         });
-        MainPanel.add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 160, 210, 30));
+        MainPanel.add(fn, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 160, 210, 30));
 
         jButton3.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 12)); // NOI18N
         jButton3.setText("REGISTER");
@@ -171,9 +174,9 @@ public class RegistrationForm extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+    private void lnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_lnActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
+    }//GEN-LAST:event_lnActionPerformed
 
     private void cancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelActionPerformed
         
@@ -182,24 +185,31 @@ public class RegistrationForm extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_cancelActionPerformed
 
-    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+    private void usernameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_usernameActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField3ActionPerformed
+    }//GEN-LAST:event_usernameActionPerformed
 
-    private void jPasswordField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField3ActionPerformed
+    private void passwordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordActionPerformed
         
-    }//GEN-LAST:event_jPasswordField3ActionPerformed
+    }//GEN-LAST:event_passwordActionPerformed
 
-    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
+    private void emailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_emailActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField4ActionPerformed
+    }//GEN-LAST:event_emailActionPerformed
 
-    private void jTextField5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField5ActionPerformed
+    private void fnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fnActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField5ActionPerformed
+    }//GEN-LAST:event_fnActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+      if (fn.getText().isEmpty() || 
+              ln.getText() .isEmpty() ||
+            username.getText().isEmpty() ||
+              password.getText().isEmpty() ){
+          
+          JOptionPane.showMessageDialog(null, "Error! Complete the Information!"); 
+           return;         
+      }          
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
@@ -240,6 +250,8 @@ public class RegistrationForm extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel MainPanel;
     private javax.swing.JButton cancel;
+    private javax.swing.JTextField email;
+    private javax.swing.JTextField fn;
     private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
@@ -249,10 +261,8 @@ public class RegistrationForm extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
-    private javax.swing.JPasswordField jPasswordField3;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField ln;
+    private javax.swing.JPasswordField password;
+    private javax.swing.JTextField username;
     // End of variables declaration//GEN-END:variables
 }
